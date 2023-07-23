5bd3697c5918df4589932714605b248148995db3
kosalanuwan <kosala.nuwan@gmail.com>
2023-07-23 18:21:42 +0530

    fix: improve equality comparer
    - ensure type comparison
    - use tuple hash code

--- src/Subs/Skol.Resthooks.Subs/Domain/ValueTypes/WebhookOptions.cs
+++ src/Subs/Skol.Resthooks.Subs/Domain/ValueTypes/WebhookOptions.cs
@@ -18,13 +18,13 @@ namespace Skol.Resthooks.Subs.Domain.ValueTypes
         public string SigningKey { get; set; }
 
         public override bool Equals(object obj)
-            => Equals(obj as WebhookOptions);
+            => obj is WebhookOptions other && Equals(other);
 
         public bool Equals(WebhookOptions other)
             => other is { } && (NotificationUrl, Version, AccessToken, SigningKey) == (other.NotificationUrl, other.Version, other.AccessToken, other.SigningKey);
 
         public override int GetHashCode()
-            => HashCode.Combine(NotificationUrl, Version, AccessToken, SigningKey);
+            => (NotificationUrl, Version, AccessToken, SigningKey).GetHashCode();
 
         public static bool operator ==(WebhookOptions left, WebhookOptions right)
             => EqualityComparer<WebhookOptions>.Default.Equals(left, right);
