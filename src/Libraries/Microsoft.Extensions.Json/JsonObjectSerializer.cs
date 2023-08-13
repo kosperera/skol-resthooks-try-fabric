30a05bff0f4101f8103d89169989c4de856dc728
kosalanuwan <kosala.nuwan@gmail.com>
2023-08-13 11:06:43 +0530

    feat(json): move nested class to serializer

--- src/Libraries/Microsoft.Extensions.Json/JsonObjectSerializer.cs
+++ src/Libraries/Microsoft.Extensions.Json/JsonObjectSerializer.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace System.Text.Json;
 
 public sealed partial class JsonObjectSerializer
@@ -14,4 +16,28 @@ public sealed partial class JsonObjectSerializer
 
     public string Serialize(object value, JsonSerializerOptions? overrideOptions = default)
         => JsonSerializer.Serialize(value, overrideOptions ?? _options);
+
+    public static class Defaults
+    {
+        public static JsonSerializerOptions JsonWebScenario(JsonSerializerOptions? options = default)
+        {
+            options ??= new JsonSerializerOptions();
+
+            options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            //options.IgnoreNullValues = true;
+
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+
+            options.PropertyNameCaseInsensitive = true;
+            options.NumberHandling = JsonNumberHandling.AllowReadingFromString;
+
+            options.AllowTrailingCommas = true;
+            options.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            options.WriteIndented = true;
+
+            return options;
+        }
+    }
+
 }
